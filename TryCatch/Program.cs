61ffd2c1a6615df381a476b8ae4dddacb4756635
Kaset4 <85079510+Kaset4@users.noreply.github.com>
2024-02-15 22:13:38 +0400

    Add files via upload

---
+++ TryCatch/Program.cs
@@ -0,0 +1,36 @@
+﻿public class CustomException : Exception
+{
+    public CustomException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Массив из пяти различных видов исключений
+        Exception[] exceptions = new Exception[]
+        {
+            new DivideByZeroException("Попытка деления на ноль."),
+            new InvalidOperationException("Недопустимая операция."),
+            new IndexOutOfRangeException("Индекс за пределами диапазона."),
+            new ArgumentException("Недопустимый аргумент."),
+            new CustomException("Произошло пользовательское исключение.")
+        };
+
+        // Реализуем конструкцию TryCatchFinally с итерацией по каждому типу исключения
+        foreach (var exception in exceptions)
+        {
+            try
+            {
+                // Генерируем исключение
+                throw exception;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Поймано исключение: " + ex.GetType().Name + ". Сообщение: " + ex.Message);
+            }
+        }
+    }
+}
